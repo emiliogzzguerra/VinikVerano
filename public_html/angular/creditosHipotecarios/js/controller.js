ff46b2363cd12610e388cdf7686d5696661e17f8
Emilio Gonzalez <emiliogzz24@gmail.com>
2016-06-23 18:47:00 -0500

    Cred hip con popup

--- public_html/angular/creditosHipotecarios/js/controller.js
+++ public_html/angular/creditosHipotecarios/js/controller.js
@@ -1,6 +1,47 @@
+
 angular.module('controller',[])
 
 .controller('CotizaCtrl', ['$scope', '$http', '$log','$window', function($scope, $http, $log,$window) {
+
+	$http.get('../angular/creditosHipotecarios/js/popData.php')
+		.success(function(data) {
+			$scope.leads = data;
+
+			console.log("Success");
+		})
+		.error(function(err) {
+			$log.error(err);
+		})
+
+	$scope.pushData = function($params) {
+		$http.post('../angular/creditosHipotecarios//js/pushData.php',{'nombre':$params.nombre, 'telefono':$params.telefono, 'email':$params.email})
+			.success(function(data) {
+				$scope.leads = data;
+				$window.location.href = 'cotizacion-cred-hipotecario.html';
+			})
+			.error(function(err) {
+				$log.error(err);
+			})
+	}
+
+	$scope.removeData = function($params) {
+		var cnfrm = confirm("Are you sure to delete?");
+		if(cnfrm) {
+			$http.post('../angular/creditosHipotecarios//js/removeData.php', {'id':$params})
+			.success(function(data) {
+				$scope.leads = data;
+			})
+			.error(function(err) {
+				$log.error(err);
+			})
+		} else {
+			//
+		}
+
+	}
+
+}])
+.controller('VarCtrl', ['$scope', '$http', '$log','$window', function($scope, $http, $log,$window) {
 
 	$http.get('../angular/creditosHipotecarios/js/popData.php')
 		.success(function(data) {
@@ -11,10 +52,10 @@ angular.module('controller',[])
 		})
 
 	$scope.pushData = function($params) {
-		$http.post('../angular/creditosHipotecarios//js/pushData.php',{'nombre':$params.nombre, 'telefono':$params.telefono, 'email':$params.email})
+		$http.post('../angular/creditosHipotecarios//js/pushVar.php',{'nombre':$params.nombre, 'telefono':$params.telefono, 'email':$params.email})
 			.success(function(data) {
 				$scope.leads = data;
-				$window.location.href = 'fondo-de-ahorro.html';
+				//$window.location.href = 'fondo-de-ahorro.html';
 			})
 			.error(function(err) {
 				$log.error(err);
--- public_html/angular/creditosHipotecarios/js/popData.php
+++ public_html/angular/creditosHipotecarios/js/popData.php
@@ -6,3 +6,44 @@
 	$data = $db->qryFire();
 
 	echo json_encode($data);
+
+	/*
+	<?php
+		include('config.php');
+
+
+
+		$db = new DB();
+		$sql = "SELECT * FROM `leads`";
+		$data = $db->qryFire();
+		echo json_encode($data);
+		
+		//$db = new DB();
+
+		// Create connection
+		$conn = new mysqli(__HOST__, __USER__, __PASS__, __BASE__);
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		}
+
+		$sql = "SELECT * FROM leads";
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+		        echo "id: " . $row["nombre"]. " - Name: " . $row["telefono"]. " " . $row["email"]. "<br>";
+		    }
+		} else {
+		    echo "0 results";
+		}
+		$conn->close();
+
+
+
+		//$data = $db->qryFire();
+
+		echo json_encode($result);
+
+*/
