556cc61565ab56101f15dec51cb1abc62955e910
Emilio Gonzalez <emiliogzz24@gmail.com>
2016-07-18 15:28:58 -0500

    Inicio cambios en CredHip

--- public_html/angular/creditosHipotecarios/js/controller.js
+++ public_html/angular/creditosHipotecarios/js/controller.js
@@ -3,6 +3,9 @@ angular.module('controller',[])
 
 
 .controller('VarCtrl', ['$scope', '$http', '$log','$window', function($scope, $http, $log,$window) {
+
+	var tope = 30000/5000;
+
 	$scope.changeChecked = function($params){
 		$scope.frm.bCoa = $params;
 	}
@@ -10,12 +13,14 @@ angular.module('controller',[])
 		$scope.frm.bCoa = $params;
 	}
 
+	$scope.pago1 = 10000;
+	$scope.pago2 = 15000;
+	$scope.ahorroMensual = 150;
+
+
 	$scope.frm = {
 		sNombre:"Emilio Gzz",
 		iEdad:32 ,
-		bCoa: 0,
-		iEdadCoa: 19,
-		bOpcCred: 1,
 		iSdoIns:2000,
 		iPago:200 ,
 		iPlazo: 12
@@ -24,13 +29,32 @@ angular.module('controller',[])
 	$scope.save = function($params) {
 		$scope.frm.sNombre = $params.sNombre;
 		$scope.frm.iEdad = $params.iEdad;
-		$scope.frm.bCoa = $params.bCoa;
-		$scope.frm.iEdadCoa = $params.iEdadCoa;
-		$scope.frm.bOpcCred = $params.bOpcCred;
 		$scope.frm.iSdoIns = $params.iSdoIns;
 		$scope.frm.iPago = $params.iPago;
 		$scope.frm.iPlazo = $params.iPlazo;
-		console.log($scope.frm);
+		$scope.pago1 = $scope.frm.iSdoIns;
+		var formula = Math.abs(($scope.frm.iSdoIns)/(((1+(0.106^-$scope.frm.iPlazo))-1)/(0.106)));
+		$scope.pago2 = Math.floor($scope.pago1-formula);
+		if ($scope.pago1 > $scope.pago2) {
+			if ($scope.pago1%5000 == 0) {
+				tope = Math.ceil($scope.pago1/5000) + 1;
+			} else {
+				tope = Math.ceil($scope.pago1/5000);
+			}
+		} else {
+			if ($scope.pago2%5000 == 0) {
+				tope = Math.ceil($scope.pago2/5000) + 1;
+			} else {
+				tope = Math.ceil($scope.pago2/5000);
+			}
+		}
+
+		$scope.ahorroMensual = Math.floor(($scope.frm.iPago)-($scope.pago2/$scope.frm.iPlazo));
+
+		console.log("pago1 = " + $scope.pago1);
+		console.log("formula = "+ formula);
+		console.log("pago2 = " + $scope.pago2);
+		chartUpdate();
 	}
 	var barChartData = {
 		labels : ["Pago"],
@@ -40,14 +64,14 @@ angular.module('controller',[])
 				strokeColor : "rgba(220,220,220,0.8)",
 				highlightFill: "rgba(220,220,220,0.75)",
 				highlightStroke: "rgba(220,220,220,1)",
-				data : [$scope.perdida]
+				data : [$scope.pago1]
 			},
 			{
 				fillColor : "rgba(151,187,205,0.5)",
 				strokeColor : "rgba(151,187,205,0.8)",
 				highlightFill : "rgba(151,187,205,0.75)",
 				highlightStroke : "rgba(151,187,205,1)",
-				data : [$scope.tiempo]
+				data : [$scope.pago2]
 			}
 		]
 
@@ -62,14 +86,14 @@ angular.module('controller',[])
 					strokeColor : "rgba(220,220,220,0.8)",
 					highlightFill: "rgba(220,220,220,0.75)",
 					highlightStroke: "rgba(220,220,220,1)",
-					data : [$scope.perdida]
+					data : [$scope.pago1]
 				},
 				{
 					fillColor : "rgba(151,187,205,0.5)",
 					strokeColor : "rgba(151,187,205,0.8)",
 					highlightFill : "rgba(151,187,205,0.75)",
 					highlightStroke : "rgba(151,187,205,1)",
-					data : [$scope.tiempo]
+					data : [$scope.pago2]
 				}
 			]
 
@@ -83,7 +107,7 @@ angular.module('controller',[])
 	    responsive : true,
 	    showTooltips: false,
 	    scaleOverride : true,
-	    scaleSteps : 6,
+	    scaleSteps : tope,
 	    scaleStepWidth : 5000,
 	    scaleStartValue : 0,
 	    onAnimationComplete: function () {
@@ -96,7 +120,7 @@ angular.module('controller',[])
 
 	        this.datasets.forEach(function (dataset) {
 	            dataset.bars.forEach(function (bar) {
-	                ctx.fillText(bar.value, bar.x, bar.y - 5);
+	                ctx.fillText("$" + bar.value, bar.x, bar.y - 5);
 	            });
 	        })
 	    }
@@ -108,14 +132,14 @@ angular.module('controller',[])
 			createGraph();
 
 			// Replace the chart canvas element
-			$('#canvas').replaceWith('<canvas id="canvas" height="100%" width="100%"></canvas>');
+			$('#canvas').replaceWith('<canvas id="canvas" height="90%" width="100%"></canvas>');
 
 			var ctx = document.getElementById("canvas").getContext("2d");
 		  window.myBar = new Chart(ctx).Bar(barChartData, {
 		    responsive : true,
 		    showTooltips: false,
 		    scaleOverride : true,
-		    scaleSteps : 6,
+		    scaleSteps : tope,
 		    scaleStepWidth : 5000,
 		    scaleStartValue : 0,
 		    onAnimationComplete: function () {
