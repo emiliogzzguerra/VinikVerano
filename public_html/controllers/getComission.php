712c04d55ed2629def1de0c0f1c0c17655b4a5ae
emiliogzzguerra <emiliogzz24@gmail.com>
2017-06-04 20:54:53 -0700

    cambio minimo nuevamente

--- public_html/controllers/getComission.php
+++ public_html/controllers/getComission.php
@@ -1,6 +1,6 @@
 <?php
 
-include($_SERVER["DOCUMENT_ROOT"] . "/conexion.php");
+include($_SERVER["DOCUMENT_ROOT"] . "conexion.php");
 include($_SERVER["DOCUMENT_ROOT"] . "/models/comission.php");
 
 //Se verifica si se ha presionado el botón de buscar, si es así se forma el query de la búsqueda
