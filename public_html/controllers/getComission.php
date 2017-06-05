3cbce7f42884024cf7b3744a79ad8b1a0fcbebd7
emiliogzzguerra <emiliogzz24@gmail.com>
2017-06-04 20:43:47 -0700

    sup

--- public_html/controllers/getComission.php
+++ public_html/controllers/getComission.php
@@ -5,21 +5,21 @@ include '../models/comission.php';
 //Se verifica si se ha presionado el botón de buscar, si es así se forma el query de la búsqueda
 
 //Se comienza con un simple select para después agregarle las condiciones de la búsqueda
-print "Ya llegue...";
+//print "Ya llegue...";
 
 $monthly_cash = $_GET['monthly_cash'];
 $years = $_GET['years'];
 $risk = $_GET['risk'];
 
-echo $monthly_cash;
+//echo $monthly_cash;
 
 $resultado_busqueda = Comission::SearchComission($monthly_cash, $years, $risk);
 
-echo $resultado_busqueda;
+//echo $resultado_busqueda;
 
 $row = mysql_fetch_assoc($resultado_busqueda);
 
-echo $resultado_busqueda;
+//echo $resultado_busqueda;
 
 echo json_encode($row);
 
