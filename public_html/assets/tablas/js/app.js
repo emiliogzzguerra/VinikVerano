caebbd9944435d6e94cb1bba28bf8c114e487b00
emiliogzzguerra <emiliogzz24@gmail.com>
2017-06-07 23:43:38 -0700

    Sigo probando json4

--- public_html/assets/tablas/js/app.js
+++ public_html/assets/tablas/js/app.js
@@ -60,7 +60,7 @@ function FondoAhorroController(VinikService){
     vm.changes = changes;
     function changes(){
         var jsonResponse = VinikService.getComission(vm.aportacionMensual,vm.aniosAhorro,vm.risk);
-        console.log(jsonResponse["data"]);
+        console.log(jsonResponse["data"].admin_cost);
 
         // InteresGanadoLogic
         if(vm.risk == 'Baja'){
